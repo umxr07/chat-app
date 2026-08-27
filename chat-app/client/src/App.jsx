e7d0191b9dd143385e59f5d6633ff2e372db0b04
umxr07 <umerskh10@gmail.com>
2026-08-27 12:43:01 +0530

    Update Socket.IO server URL

--- chat-app/client/src/App.jsx
+++ chat-app/client/src/App.jsx
@@ -14,7 +14,7 @@ import "./App.css";
 // SOCKET
 // =====================================================
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://chatly-q2yn.onrender.com", {
     autoConnect: true
 });
 
